5b743a6dff3ada0193b8408706c3dd1b5006557f
Julia <>
2022-07-20 16:13:31 +0200

    Cleaned code :recycle:

--- src/js/init.js
+++ src/js/init.js
@@ -3,6 +3,7 @@ import * as d3 from "d3";
 import $ from "jquery";
 import images from "../img/*.png";
 
+
 const windowWidth = $(window).width();
 const windowHeight = $(window).height();
 const parentWidth = $("#viz").parent().width();
@@ -13,6 +14,7 @@ $(window).resize(function () {
         return;
     }
 });
+
 
 const width = parentWidth >= 1000 ? 1000 : parentWidth;
 const height = width;
@@ -56,7 +58,7 @@ const svg = d3.select("#viz")
 
 const g = svg.append("g")
     .attr("transform", `translate(${width / 2}, ${height / 2})`);
-//-100 to reduce the distance between the top of the viz and the buttons
+    //-100 to reduce the distance between the top of the viz and the buttons
 const circleG = g.append("g")
     .attr("id", "circleG");
 
@@ -72,7 +74,7 @@ const label_radius = screenSize == "medium" || screenSize == "small"? width * 0.
 const line_radius1 = screenSize == "medium" || screenSize == "small"? width * 0.46:width * 0.38;
 const line_radius2 = screenSize == "medium" || screenSize == "small"? width * 0.5:width * 0.43;
 
-//wrap text
+//function to wrap text
 const wrap = (text, width) => {
     text.each(function () {
         var text = d3.select(this),
@@ -98,6 +100,30 @@ const wrap = (text, width) => {
     });
 }
 
+// function to add attributes to nodes
+const addAttributes = (data) =>{
+
+    data.forEach(function (d, i) {
+        d.cluster = i;
+        d.startAngle = d.centerAngle - mango_angle_distance / 2;
+        d.endAngle = d.centerAngle + mango_angle_distance / 2;
+        d.startAngle2 = d.centerAngle - mango_angle_distance / 2 + mango_angle_distance / 12;
+        d.endAngle2 = d.centerAngle + mango_angle_distance / 2 - mango_angle_distance / 12;
+        d.x = circle_radius * Math.cos(data[d.cluster].centerAngle - PI05);
+        d.y = circle_radius * Math.sin(data[d.cluster].centerAngle - PI05);
+    })
+    return data
+}
+
+// function to calculate arc paths
+const calculatePath = (startAngle2,endAngle2) =>{
+    var rad = arc_radius,
+        xs = rad * Math.cos(startAngle2 - PI05),
+        ys = rad * Math.sin(startAngle2 - PI05),
+        xt = rad * Math.cos(endAngle2 - PI05),
+        yt = rad * Math.sin(endAngle2 - PI05);
+    return "M" + xs + "," + ys + " A" + rad + "," + rad + " 0 0 1 " + xt + "," + yt;
+}
 
 //DATA CLEANING
 
@@ -106,32 +132,20 @@ const root = d3.stratify()
     .parentId(d => d.parent)
     (mangoes);
 
-const cluster = d3.cluster()
-    .size([360, null]); // the second var doesn't seem to matter
+const cluster = d3.cluster().size([360, null]);
 
-const mangoData = cluster(root).leaves();
-// root.children seems to be the same thing, but only works if I have console.log(cluster(root).leaves())... b/c I need the x & y
-// console.log(cluster(root).leaves())
-// console.log(root.children)
+let mangoData = cluster(root).leaves();
 
 mangoData.forEach(function (d, i) {
     d.centerAngle = (d.x - centerAdjustment) * Math.PI / 180; //-centerAdjustment so it starts at the center at top...basically centerAngle is just x converted from 360 to radians
 });
 
-const mango_angle_distance = mangoData[1].centerAngle - mangoData[0].centerAngle;
+const mango_angle_distance = mangoData[1].centerAngle - mangoData[0].centerAngle; // for 23 mangoes
 
-mangoData.forEach(function (d, i) {
-    d.startAngle = d.centerAngle - mango_angle_distance / 2;
-    d.endAngle = d.centerAngle + mango_angle_distance / 2;
-    d.startAngle2 = d.centerAngle - mango_angle_distance / 2 + mango_angle_distance / 12;
-    d.endAngle2 = d.centerAngle + mango_angle_distance / 2 - mango_angle_distance / 12;
-    d.cluster = i;
-    d.x = circle_radius * Math.cos(mangoData[d.cluster].centerAngle - PI05);
-    d.y = circle_radius * Math.sin(mangoData[d.cluster].centerAngle - PI05);
-}) //forEach
+mangoData = addAttributes(mangoData);
 
-
-const circles = circleG.selectAll("image.mango")
+// join the mango images
+circleG.selectAll("image.mango")
     .data(mangoData)
     .join("svg:image")
     .attr("class", "mango")
@@ -193,8 +207,7 @@ circleG.append("text")
     .style("opacity", 0)
     .text("");
 
-//Label
-
+//Label line, arc & text
 mangoNameG.append("line")
     .attr("class", "labelLine")
     .attr("x1", line_radius1 * Math.cos(centerAdjustment * Math.PI / 180 - mango_angle_distance / 2 - PI05))
@@ -244,14 +257,7 @@ mangoNameG.selectAll(".mangoArc")
     .style("stroke", "#c4c4c4")
     .style("stroke-width", "1px")
     .style("fill", "none")
-    .attr("d", function (d, i) {
-        var rad = arc_radius,
-            xs = rad * Math.cos(d.startAngle2 - PI05),
-            ys = rad * Math.sin(d.startAngle2 - PI05),
-            xt = rad * Math.cos(d.endAngle2 - PI05),
-            yt = rad * Math.sin(d.endAngle2 - PI05)
-        return "M" + xs + "," + ys + " A" + rad + "," + rad + " 0 0 1 " + xt + "," + yt;
-    });
+    .attr("d", d => calculatePath(d.startAngle2, d.endAngle2));
 
 
 mangoNameG.selectAll(".mangoText")
@@ -271,63 +277,34 @@ mangoNameG.selectAll(".mangoText")
     .text(d => d.data.name_en);
 
 
-const sortMangoByAttribute = (attribute) => {
+const sortMangoByAttribute = () => {
 
-    const sortedByAttr = root.sort((a, b) => d3.ascending(a.data[attribute], b.data[attribute]))
-    const mangoDataAttr = cluster(sortedByAttr).leaves()
-    mangoDataAttr.forEach(function (d, i) {
-        d.cluster = i;
-        d.centerAngle = (d.x - centerAdjustment) * Math.PI / 180; //basically centerAngle is just x converted from 360 to radians
-        d.startAngle = d.centerAngle - mango_angle_distance / 2;
-        d.endAngle = d.centerAngle + mango_angle_distance / 2;
-        d.startAngle2 = d.centerAngle - mango_angle_distance / 2 + mango_angle_distance / 12;
-        d.endAngle2 = d.centerAngle + mango_angle_distance / 2 - mango_angle_distance / 12;
-        d.x = circle_radius * Math.cos(mangoDataAttr[d.cluster].centerAngle - PI05);
-        d.y = circle_radius * Math.sin(mangoDataAttr[d.cluster].centerAngle - PI05);
-    }) //forEach
+    let sortedByAttr; //sort root baed on attribute
+    if(mode==='size'){
+        sortedByAttr = root.sort((a, b) => d3.ascending(a.data.size_cm, b.data.size_cm))
+    }else{
+        sortedByAttr = root.sort((a, b) => d3.ascending(a.data.sweetness_brix, b.data.sweetness_brix))
+    }
 
-    return mangoDataAttr;
+    let mangoDataSorted = cluster(sortedByAttr).leaves()
+
+    mangoDataSorted.forEach(function (d, i) {
+        d.centerAngle = (d.x - centerAdjustment) * Math.PI / 180;
+    })
+
+    return addAttributes(mangoDataSorted);
 }
-
-$('#size').on('click', function () {
-    const mangoDataSize = sortMangoByAttribute("size_cm");
-    mode = 'size';
-    $("#size").addClass("active");
-    $("#sweetness").removeClass("active");
-    drawSortedMangoes(mangoDataSize);
-});
-
-$('#sweetness').on('click', function () {
-    const mangoDataSweetness = sortMangoByAttribute("sweetness_brix");
-    mode = 'sweet';
-    $("#sweetness").addClass("active");
-    $("#size").removeClass("active");
-    drawSortedMangoes(mangoDataSweetness);
-});
-
 
 
 const drawSortedMangoes = (data) => {
 
-    circleG.select("#text1")
-        .transition().duration(500)
-        .text("");
-
-    circleG.select("#text2")
-        .transition().duration(500)
-        .text("");
-
-    circleG.select("#text3")
-        .transition().duration(500)
-        .text("");
-
-    circleG.select("#text4")
+    circleG.selectAll("#text1,#text2,#text3,#text4")
         .transition().duration(500)
         .text("");
 
     d3.selectAll("image.mango")
         .data(data, d => d.id)
-        .join("svg:image")
+        .join("image")
         .attr("class", "mango")
         .transition()
         .delay((d, i) => 50 * i)
@@ -338,36 +315,24 @@ const drawSortedMangoes = (data) => {
         .attr("height", imgWidth)
         .attr("xlink:href", d =>`${images[d.data.name]}`)
 
-    //if I add click on a mango so that it goes to the center, then click on sortBySize, the last arc doesn't draw if I use d3.selectAll as opposed to mangoNameG.selectAll
 
     mangoNameG.selectAll(".mangoArc")
         .data(data)
-        .join("path")
-        .attr("class", "mangoArc")
-        .style("stroke", "#c4c4c4")
-        .style("stroke-width", "1px")
-        .style("fill", "none")
+        .join(
+        enter => enter.append("path")
+            .attr("class", "mangoArc")
+            .attr("class", "mangoArc")
+            .style("stroke", "#c4c4c4")
+            .style("stroke-width", "1px")
+            .style("fill", "none")
+        )
         .attr("id", (d, i) => `mango_${i}`)
         .transition()
         .delay((d, i) => 50 * i)
         .duration(1000)
-        .attr("d", function (d, i) {
-            var rad = arc_radius,
-                xs = rad * Math.cos(d.startAngle2 - PI05),
-                ys = rad * Math.sin(d.startAngle2 - PI05),
-                xt = rad * Math.cos(d.endAngle2 - PI05),
-                yt = rad * Math.sin(d.endAngle2 - PI05)
-            return "M" + xs + "," + ys + " A" + rad + "," + rad + " 0 0 1 " + xt + "," + yt;
-        })
+        .attr("d", d => calculatePath(d.startAngle2, d.endAngle2))
 
-    //if I add click on a mango so that it goes to the center, then click on sortBySize, then the last mango text doesn't draw,
-    // even though the data still shows 23 mangoes, the d.data.name for the mangoText only shows 22
-
-
-    // mangoNameG.selectAll(".mangoText textPath").remove().
-
-    //  currently only includes the update elements, not the enter elements
-    mangoNameG.selectAll(".mangoText")
+        mangoNameG.selectAll(".mangoText")
         .data(data)
         .join(
             enter => enter.append("text")
@@ -377,8 +342,6 @@ const drawSortedMangoes = (data) => {
                 .attr("class", "mangoText")
                 .select("textPath")
         )
-        // .attr("class", "mangoText") // still 23 items here
-        // .select("textPath") // becomes 22 items. why? because I need to append the last one, but not sure how to do it
         .attr("startOffset", "50%")
         .style("text-anchor", "middle")
         .style("font-weight", 400)
@@ -392,18 +355,15 @@ const drawSortedMangoes = (data) => {
         .duration(1000)
         .attr("xlink:href", (d, i) => "#mango_" + i)
         .text(d => d.data.name_en);
-
-    //SEPARATE THE ABOVE FOR UPDATE vs ENTER ELEMENTS, FOR UPDATE USE SELECT, FOR ENTER USE APPEND
-
+        //SEPARATE THE ABOVE FOR UPDATE vs ENTER ELEMENTS, FOR UPDATE USE SELECT, FOR ENTER USE APPEND
 }
 
 const rearrageMangoData = (data) => {
 
-
     const filtered_hierarchy = mangoes.filter(d => d.name !== data.name);
     // filter the selected mango out since it will be moved to the center
     const root2 = d3.stratify()
-        .id(d => d.name)  //any column that's unique
+        .id(d => d.name)
         .parentId(d => d.parent)
         (filtered_hierarchy);
 
@@ -416,24 +376,19 @@ const rearrageMangoData = (data) => {
     // when the user clicks on a mango it won't sort it by sweetness before moving the mango to the middle
 
     //before cluster,the children's includes the following attributes...
-    // data: {parent: 'All', name: '夏雪', name_en: 'XiaXue', month start: 6, month end: 7, …}
+    // data: {parent: 'All', name: '夏雪', name_en: 'XiaXue' ...}
     // depth: 1
     // height: 0
     // id: "夏雪"
 
     cluster(rootSorted);
 
-    //after cluster, the children's includes the following attributes...
-    // data: {parent: 'All', name: '夏雪', name_en: 'XiaXue', month start: 6, month end: 7, …}
-    // depth: 1
-    // height: 0
-    // id: "夏雪"
-    // parent: pd {data: {…}, height: 1, depth: 0, parent: null, id: 'All', …}
+    //after cluster, the children's also has parent, x & y
+    // parent: pd {data: {…}, height: 1, depth: 0, parent: null, id: 'All'...}
     // x: 8.181818181818182
     // y: 0
 
-
-    const filteredData = rootSorted.children.filter(d => d.data.name !== data.name);
+    let filteredData = rootSorted.children.filter(d => d.data.name !== data.name);
     const selectedData = root.children.filter(d => d.data.name === data.name);
 
     d3.map(selectedData, function (d) {
@@ -443,51 +398,32 @@ const rearrageMangoData = (data) => {
     })
 
     filteredData.forEach(function (d, i) {
-        d.centerAngle = (d.x - centerAdjustment) * Math.PI / 180; //basically centerAngle is just x converted from 360 to radians
+        d.centerAngle = (d.x - centerAdjustment) * Math.PI / 180;
     });
 
     const mango_angle_distance = filteredData[1].centerAngle - filteredData[0].centerAngle;
+    filteredData =  addAttributes(filteredData,mango_angle_distance);
 
-    filteredData.forEach(function (d, i) {
-        d.cluster = i;
-        d.startAngle = d.centerAngle - mango_angle_distance / 2;
-        d.endAngle = d.centerAngle + mango_angle_distance / 2;
-        d.startAngle2 = d.centerAngle - mango_angle_distance / 2 + mango_angle_distance / 12;
-        d.endAngle2 = d.centerAngle + mango_angle_distance / 2 - mango_angle_distance / 12;
-        d.x = circle_radius * Math.cos(filteredData[d.cluster].centerAngle - PI05);
-        d.y = circle_radius * Math.sin(filteredData[d.cluster].centerAngle - PI05);
-    }) //forEach
-
-    //after the above step, the children includes the following attributes:
+    //after the above step, the children includes also the following attributes, with x and y updated:
     // centerAngle: 0.27369935997183803
     // cluster: 0
-    // data: {parent: 'All', name: '夏雪', name_en: 'XiaXue', month start: 6, month end: 7, …}
-    // depth: 1
     // endAngle: 0.4164990260441014
     // endAngle2: 0.39269908169872414
-    // height: 0
-    // id: "夏雪"
-    // parent: pd {data: {…}, height: 1, depth: 0, parent: null, id: 'All', …}
     // startAngle: 0.13089969389957468
     // startAngle2: 0.1546996382449519
     // x: 72.97963350023863
     // y: -259.94994343944535
 
-
     return [filteredData, filteredData.concat(selectedData)];
-
-
 }
 
 
 const mangoClicked = (dataFiltered, data) => {
-    //dataFiltered doesn't include the mango in the middle, so can use it for the labels
 
     const selected = data.filter(d => d.cluster === 22);
-    console.log(selected)
 
     d3.selectAll("image.mango")
-        .data(data, d => d.id) //d=>d.cluster
+        .data(data, d => d.id)
         .join("svg:image")
         .attr("class", "mango")
         .transition()
@@ -497,6 +433,8 @@ const mangoClicked = (dataFiltered, data) => {
         .attr("width", d => d.cluster === 22 ? imgWidth + imgIncrease : imgWidth)
         .attr("height", d => d.cluster === 22 ? imgWidth + imgIncrease : imgWidth)
         .attr("xlink:href", d => `${images[d.data.name]}`)
+
+    //dataFiltered doesn't include the mango in the middle, so can use it for the labels
 
     d3.selectAll(".mangoArc")
         .data(dataFiltered)
@@ -509,14 +447,7 @@ const mangoClicked = (dataFiltered, data) => {
         .transition()
         .delay((d, i) => 20 * i)
         .duration(1000)
-        .attr("d", function (d, i) {
-            var rad = arc_radius,
-                xs = rad * Math.cos(d.startAngle2 - PI05),
-                ys = rad * Math.sin(d.startAngle2 - PI05),
-                xt = rad * Math.cos(d.endAngle2 - PI05),
-                yt = rad * Math.sin(d.endAngle2 - PI05)
-            return "M" + xs + "," + ys + " A" + rad + "," + rad + " 0 0 1 " + xt + "," + yt;
-        });
+        .attr("d", d => calculatePath(d.startAngle2, d.endAngle2));
 
 
     d3.selectAll(".mangoText")
@@ -533,6 +464,8 @@ const mangoClicked = (dataFiltered, data) => {
         .attr("xlink:href", (d, i) => "#mango_" + i)
         .style("opacity", 1)
         .text(d => d.data.name_en);
+
+    // Update the text in the middle
 
     circleG.select("#text1")
         .datum(selected[0])
@@ -588,3 +521,19 @@ const mangoClicked = (dataFiltered, data) => {
         })
         .call(wrap, windowWidth <= 992 ? 150 : 250);
 }
+
+$('#size').on('click', function () {
+    mode = 'size';
+    const mangoDataSize = sortMangoByAttribute();
+    $("#size").addClass("active");
+    $("#sweetness").removeClass("active");
+    drawSortedMangoes(mangoDataSize);
+});
+
+$('#sweetness').on('click', function () {
+    mode = 'sweet';
+    const mangoDataSweetness = sortMangoByAttribute();
+    $("#sweetness").addClass("active");
+    $("#size").removeClass("active");
+    drawSortedMangoes(mangoDataSweetness);
+});
